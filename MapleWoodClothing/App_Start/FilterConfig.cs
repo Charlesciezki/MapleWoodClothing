752628dc2417fecf9ef0abc78b9fbbdd25f5612a
Charlesciezki <charlesciezki@yahoo.com>
2017-07-10 14:42:25 -0500

    init commit

---
+++ MapleWoodClothing/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MapleWoodClothing
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MapleWoodClothing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MapleWoodClothing.Startup))]
+namespace MapleWoodClothing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
